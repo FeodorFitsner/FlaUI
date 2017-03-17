b2dfbe0b33a308db46faa294ad5487812f2f79c4
Feodor Fitsner <feodor@appveyor.com>
2017-03-16 23:17:07 -0700

    Rename 3rd DLL

--- RunTests.bat
+++ RunTests.bat
@@ -3,6 +3,7 @@ rem nunit3-console "src\FlaUI.Core.UnitTests\bin\FlaUI.Core.UnitTests.dll" --res
 rem SET /A errno=%errno%+%ERRORLEVEL%
 nunit3-console "src\FlaUI.Core.UITests\bin\FlaUI.Core.UITests.dll" --params=uia=2 --result=UIA2TestResult.xml
 SET /A errno=%errno%+%ERRORLEVEL%
-nunit3-console "src\FlaUI.Core.UITests\bin\FlaUI.Core.UITests.dll" --params=uia=3 --result=UIA3TestResult.xml
+ren "src\FlaUI.Core.UITests\bin\FlaUI.Core.UITests-3.dll"
+nunit3-console "src\FlaUI.Core.UITests\bin\FlaUI.Core.UITests-3.dll" --params=uia=3 --result=UIA3TestResult.xml
 SET /A errno=%errno%+%ERRORLEVEL%
 EXIT /B %errno%
